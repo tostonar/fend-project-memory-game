64b604075f5d82680d6a85a3d4049a153bfcef4c
Andrea Toston <tostonar@gmail.com>
2018-05-28 23:18:25 -0400

    Created TODO list

--- js/app.js
+++ js/app.js
@@ -55,11 +55,11 @@ restartButton.addEventListener('click', function(event) {
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ // TODO:   - if the list already has another card, check to see if the two cards match
+ // TODO:     + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ // TODO:     + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ // TODO:     + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ // TODO:     + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
 // display card's symbol
@@ -73,7 +73,7 @@ function addCard(card) {
   openCards.push(card);
 }
 
-// show card ( add .show to li) on click
+// on click, show card, add card to list
 for (const card of cards) {
   card.addEventListener('click', function(event) {
     if (event.target.nodeName === 'LI') {
