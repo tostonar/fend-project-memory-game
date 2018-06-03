c64e07b8a8ad4613e4bfeaca664d392020b05f22
Andrea Toston <tostonar@gmail.com>
2018-06-03 11:44:22 -0400

    can match cards, know when game is won, has counter for moves

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
+const cards = document.querySelectorAll('.card');
+let openCards = [];
+let moves = 0;
+const displayMoves = document.querySelector('.moves');
+const star = '<li><i class="fa fa-star"></i></li>';
+let stars = document.querySelectorAll('.fa-star');
 /*
  * Create a list that holds all of your cards
  */
-const cards = document.querySelectorAll('li.card');
+
 
 // convert NodeList to array
 let arrayOfCards =  Array.from(cards);
@@ -40,7 +46,13 @@ function createDeck() {
   for (const item of arrayOfCards) {
     // add each card's HTML to the page
   	deck.appendChild(item);
+    // remove match class
+    item.classList.remove('match');
   }
+  // reset timer and moves and stars
+  moves = 0;
+  displayMoves.textContent = `${moves}`;
+  document.querySelector('.stars').innerHTML = star.repeat(3);
 }
 
 // add createDeck() to restart button
@@ -55,44 +67,107 @@ restartButton.addEventListener('click', function(event) {
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- // TODO:   - if the list already has another card, check to see if the two cards match
- // TODO:     + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- // TODO:     + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- // TODO:     + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ //    - if the list already has another card, check to see if the two cards match
+ //      + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ //      + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ //      + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  // TODO:     + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ // currently just using window.alert; so basic
+ //          maybe create a modal, or dialog html element
+ // TODO:     timer must start when first card is clicked, and stop when all cards are matched, and be displayed in message with final score, and start over when reset button is clicked    timer: try setInterval()
+//  deal with stars, like 3 stars for 20 moves or less, 2 stars for 21-25, and 1 star for 26+ moves
+// TODO: new problem - with restart, moves need to go back to 0, stars need to start at 3 showing, and cards need to be hidden(remove match class)
  */
 
-// display card's symbol
+
+// display card
 function showCard(card) {
-  card.classList.toggle('show');
+  card.classList.add('show', 'open');
 }
 
 // add card to list of open cards
-// TODO: problem - how to remove cards that aren't currently shown
-let openCards = [];
 function addCard(card) {
-  if (card.classList.contains('show')) {
-    openCards.push(card);
-  }
+  openCards.push(card);
 }
 
-// if list has another card, check to see if they matched
-function checkForMatch() {
-  if (openCards.length === 2) {
-    if (openCards[0].firstElementChild.classList[1] === openCards[1].firstElementChild.classList[1]) {
-      console.log("It's a match!")
-    } else {
-      console.log("It's not a match! Keep looking.")
-    }
-  }
+// if cards match, lock in position
+function cardMatch(card) {
+  card.classList.add('match');
+  card.classList.remove('open');
+  card.classList.remove('show');
 }
 
-// on click, show card, add card to list
-for (const card of cards) {
-  card.addEventListener('click', function(event) {
-    if (event.target.nodeName === 'LI') {
-      showCard(event.target);
-    }
-    addCard(card);
+// if cards don't match, hide cards
+function hideCard(card) {
+  card.classList.remove('open', 'show');
+}
+
+// increment moves and display
+function increaseCount() {
+  moves += 1;
+  displayMoves.textContent = `${moves}`;
+}
+
+// stars
+
+function displayStars() {
+  stars = document.querySelectorAll('.fa-star');
+  if (moves > 25) {
+    // show 1 star
+    stars[0].remove();
+    stars[1].remove();
+  } else if (moves > 20 && moves < 26) {
+    // show 2 stars
+    stars[0].remove();
+
+  }
+  stars = document.querySelectorAll('.fa-star');
+}
+
+
+// when game is over
+function gameOver(array) {
+
+  // if all cards have match class then
+  let didYouWin = array.every(function (e) {
+    return e.classList.contains('match');
   });
+  return didYouWin;
 }
+
+
+
+cards.forEach(function(card) {
+  card.addEventListener('click', function(event) {
+    if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+      addCard(card);
+      showCard(card);
+
+      if (openCards.length >= 2) {
+        // Check if cards match
+        if (openCards[0].dataset.card === openCards[1].dataset.card) {
+          openCards.forEach(function(card) {
+            cardMatch(card);
+          });
+          openCards = [];
+        } else {
+          // If cards don't match
+          setTimeout(function() {
+            openCards.forEach(function(card) {
+              hideCard(card);
+            });
+            openCards = [];
+          }, 1000);
+        }
+      }
+      increaseCount();
+
+      if (gameOver(arrayOfCards)) {
+        displayStars();
+        setTimeout(function() {
+          window.alert(`Congratulations!! You won in only ${moves} moves, so you get ${stars.length} star(s)!!`);
+        }, 500);
+      }
+    }
+  });
+});
